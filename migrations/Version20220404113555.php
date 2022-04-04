f82c0e9bd31354a5899eaf2a0bf2163fb344a7ac
d.arishev <sividmitry@gmail.com>
2022-04-04 14:44:42 +0300

    Configure database sessions

---
+++ migrations/Version20220404113555.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:ignoreFile
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220404113555 extends AbstractMigration
+{
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function getDescription(): string
+    {
+        return 'TODO: Describe reason for this migration';
+    }
+
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     */
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C604B8382 FOREIGN KEY (conference_id) REFERENCES conference (id)');
+    }
+
+    /**
+     * @noinspection PhpMissingParentCallCommonInspection
+     *
+     * @throws Exception
+     */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() !== 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C604B8382');
+    }
+}
